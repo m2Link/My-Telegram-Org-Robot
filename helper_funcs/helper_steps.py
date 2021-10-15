2c2aecf5800c2998e2e9d94b6d7372118ef33918
m2Link <88938300+m2Link@users.noreply.github.com>
2021-10-15 16:13:20 +0530

    Update helper_steps.py

--- helper_funcs/helper_steps.py
+++ helper_funcs/helper_steps.py
@@ -32,39 +32,19 @@ def parse_to_meaning_ful_text(input_phone_number, in_dict):
     """ convert the dictionary returned in STEP FOUR
     into Telegram HTML text """
     me_t = ""
-    me_t += "<i>Phone Number</i>: "
+    me_t += "<i>📱Phone Number</i>: "
     me_t += f"<u>{input_phone_number}</u>"
     me_t += "\n"
     me_t += "\n"
-    me_t += "<i>App Configuration</i>"
     me_t += "\n"
-    me_t += "<b>APP ID</b>: "
-    me_t += "<code>{}</code>".format(in_dict["App Configuration"]["app_id"])
-    me_t += "\n"
-    me_t += "<b>API HASH</b>: "
-    me_t += "<code>{}</code>".format(in_dict["App Configuration"]["api_hash"])
+    me_t += "<b>❣️APP ID</b>👇"
+    me_t += "\n<code>{}</code>".format(in_dict["App Configuration"]["app_id"])
     me_t += "\n"
     me_t += "\n"
-    me_t += "<i>Available MTProto Servers</i>"
     me_t += "\n"
-    me_t += "<b>Production Configuration</b>: "
-    me_t += "<code>{}</code> <u>{}</u>".format(
-        in_dict["Available MTProto Servers"]["production_configuration"]["IP"],
-        in_dict["Available MTProto Servers"]["production_configuration"]["DC"]
-    )
-    me_t += "\n"
-    me_t += "<b>Test Configuration</b>: "
-    me_t += "<code>{}</code> <u>{}</u>".format(
-        in_dict["Available MTProto Servers"]["test_configuration"]["IP"],
-        in_dict["Available MTProto Servers"]["test_configuration"]["DC"]
-    )
-    me_t += "\n"
-    me_t += "\n"
-    me_t += "<i>Disclaimer</i>: "
-    me_t += "<u>{}</u>".format(
-        in_dict["Disclaimer"]
-    )
-    return me_t
+    me_t += "<b>❣️API HASH</b>👇"
+    me_t += "\n<code>{}</code>".format(in_dict["App Configuration"]["api_hash"])
+        return me_t
 
 
 def extract_code_imn_ges(ptb_message):
